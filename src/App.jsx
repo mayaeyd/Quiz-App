6041611bac65f0bdbbbe68fcc37ccb5291b1cd69
Maya Eid <mayabaseleid@gmail.com>
2024-12-04 21:30:03 +0200

    Add some styling to welcome page

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,17 @@
-//import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { useEffect } from "react";
 import "./styles/App.css";
 import { fetchQuizzes } from "./redux/slices/quizSlices";
 import { useDispatch, useSelector } from "react-redux";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import WelcomePage from "./pages/WelcomePage/WelcomePage";
+import Quiz from "./pages/Quiz/Quiz";
 
 function App() {
   return(
     <BrowserRouter>
       <Routes>
         <Route path="/" element= {<WelcomePage/>} />
+        <Route path="/quiz" element= {<Quiz />} />
       </Routes>
     </BrowserRouter>
   );
