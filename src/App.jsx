d619e102729607feb9f03c2e2cfb5ca198306d49
Maya Eid <mayabaseleid@gmail.com>
2024-12-04 21:04:57 +0200

    Create welcome page

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,16 @@ import { useEffect } from "react";
 import "./styles/App.css";
 import { fetchQuizzes } from "./redux/slices/quizSlices";
 import { useDispatch, useSelector } from "react-redux";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import WelcomePage from "./pages/WelcomePage";
 
 function App() {
-  const {} = useSelector((state)=>state.quizzes)
-  const dispatch = useDispatch();
-
-  useEffect(()=>{
-    dispatch(fetchQuizzes());
-  },[])
-  return (
-    <>
-
-    </>
+  return(
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element= {<WelcomePage/>} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/WelcomePage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const WelcomePage = () => {
+  return (
+    <div>
+      GET STARTED
+    </div>
+  )
+}
+
+export default WelcomePage
