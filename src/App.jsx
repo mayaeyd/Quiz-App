2ab83e8b8e915194573bd8399a518b66787e6ba6
Maya Eid <mayabaseleid@gmail.com>
2024-12-04 20:04:55 +0200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./styles/App.css";
+
+function App() {
+  return (
+    <div>
+      Hello
+    </div>
+  );
+}
+
+export default App;
