3243d7f77deeb9e1280d7b22e2f8e9ac850ce298
Maya Eid <mayabaseleid@gmail.com>
2024-12-04 20:08:17 +0200

    Provide redux store to app

--- src/index.js
+++ src/index.js
@@ -2,7 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./styles/index.css";
 import App from "./App.jsx";
+import { Provider } from "react-redux";
+import store from "../redux/store.js";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <App />
+  <Provider store={store}>
+    <App />
+  </Provider>
 );
